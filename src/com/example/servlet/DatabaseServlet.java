7f777834921c7b28e28f09db7b728e449af57731
lihao <805459342@qq.com>
2024-12-03 20:49:56 +0800

    数据库连接

--- src/com/example/servlet/DatabaseServlet.java
+++ src/com/example/servlet/DatabaseServlet.java
@@ -15,7 +15,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * classname
+ * 数据库测试
  *
  * @author lihao
  * &#064;date  2024/12/3--20:32
@@ -30,13 +30,10 @@ public class DatabaseServlet extends HttpServlet {
         // 设置响应内容类型
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-
         // 获取数据库连接
         Connection conn = DBConfig.getConnection();
-
         if (conn != null) {
             try {
-                // 查询数据库（假设有一个 users 表）
                 String query = "SELECT * FROM users";
                 PreparedStatement statement = conn.prepareStatement(query);
                 ResultSet resultSet = statement.executeQuery();
